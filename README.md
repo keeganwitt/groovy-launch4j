f7d4b1cf4d49622efda481c5bf8a007b1445ada8
Keegan Witt <keeganwitt@gmail.com>
2016-09-25 18:36:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# groovy-launch4j
+Experimental replacement for [Groovy native launcher](https://github.com/groovy/groovy-native-launcher) binaries.
+
+## Instructions
+1. Run build.bat.
+
+## Prerequisites
+* [Launch4j](http://launch4j.sourceforge.net/)
---
+++ build.bat
@@ -0,0 +1,11 @@
+mkdir build
+
+launch4jc src\launch4j\groovy.xml
+launch4jc src\launch4j\groovyw.xml
+launch4jc src\launch4j\groovyConsole.xml
+launch4jc src\launch4j\groovyc.xml
+launch4jc src\launch4j\groovysh.xml
+launch4jc src\launch4j\java2groovy.xml
+launch4jc src\launch4j\grape.xml
+launch4jc src\launch4j\gant.xml
+launch4jc src\launch4j\gantw.xml
